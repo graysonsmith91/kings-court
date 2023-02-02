172027cf74bd90d769227294d931387de6ccec83
Grayson Smith <graysonsmith91@gmail.com>
2023-02-02 11:25:50 -0600

    added create a post functionality and expanded post details with working delete button

--- src/components/posts/PostList.js
+++ src/components/posts/PostList.js
@@ -4,10 +4,11 @@ import "./Posts.css"
 
 export const PostList = () => {
     const [posts, setPosts] = useState([])
+    const navigate = useNavigate()
 
     useEffect(
         () => {
-            fetch(`http://localhost:8088/posts`)
+            fetch(`http://localhost:8088/posts?_expand=category`)
                 .then(res => res.json())
                 .then((postsArray) => {
                     setPosts(postsArray)
@@ -18,18 +19,30 @@ export const PostList = () => {
 
     return <>
         <h2>Posts</h2>
+        <div className="createPostDiv">
+            <button className="createPost-button" onClick={(clickEvent) => navigate("/create")}>Create a Post</button>
+        </div>
 
         <div className="posts">
             {
                 posts.map(
                     (post) => {
-                        return <div className="post">
-                            {post.text}
-                        </div>
+                        return <>
+                            <div className="post" onClick={() => navigate(`/post/${post.id}`)} key={post.id}>
+                                <div className="category">
+                                    {post.category.category}
+                                </div>
+                                {post.text}
+
+                            </div>
+                        </>
                     }
                 )
+
             }
 
         </div>
     </>
 }
+
+// TODO: Can change this map to the PostDetails function and use props from it I think
